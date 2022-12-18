c15b49ad908608884463bbbfc9b4901c826dc096
Ahmed Atef <atteff101@gmail.com>
2022-12-19 01:56:22 +0200

    Update rank.ts

--- server/src/controllers/rank.ts
+++ server/src/controllers/rank.ts
@@ -1,8 +1,7 @@
-// i put outside the getRank controller to be like a database (each time i add a score it keeps it) (saved in cash if server restarted it will be lost).
-
 import { RequestHandler } from 'express';
 import { Error, rankBody } from './types';
 
+// i put outside the getRank controller to be like a database (each time i add a score it keeps it) (saved in cash if server restarted added new data will be lost).
 const dummy_ranks = [
   20, 90, 100, 50, 10, 50, 60, 0, 60, 10, 90, 30, 100, 30, 20, 90, 40, 20, 10,
   60, 50, 100, 50, 80, 50, 80, 60, 80, 10, 40,
